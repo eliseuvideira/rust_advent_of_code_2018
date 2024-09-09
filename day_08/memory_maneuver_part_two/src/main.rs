72008e40dc85bf2ba65bcbe8c36078bd041d8d4a
Eliseu Videira <eliseu@tryfinch.com>
2024-09-08 22:03:08 -0300

    feat: memory_maneuver_part_two

---
+++ day_08/memory_maneuver_part_two/src/main.rs
@@ -0,0 +1,93 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
+
+    for line in lines {
+        let instructions: Vec<u8> = line
+            .split(' ')
+            .filter_map(|entry| entry.parse::<u8>().ok())
+            .collect();
+
+        let root_node = build_node(&instructions);
+
+        let sum = calc_node(&root_node);
+
+        println!("{}", sum);
+    }
+
+    Ok(())
+}
+
+fn build_node_recurse(instructions: &Vec<u8>, start_index: usize, id: u8) -> (usize, Node) {
+    let child_nodes_len = instructions.get(start_index).expect("child nodes len");
+    let metadata_entries_len = instructions
+        .get(start_index + 1)
+        .expect("metadata entries len");
+
+    let mut child_nodes: Vec<Node> = vec![];
+    let mut metadata_entries: Vec<u8> = vec![];
+
+    let mut index = start_index + 2;
+    for c in 0..*child_nodes_len {
+        let (current_index, node) = build_node_recurse(instructions, index, id + c + 1);
+
+        child_nodes.push(node);
+        index = current_index;
+    }
+
+    for _ in 0..*metadata_entries_len {
+        if let Some(metadata_entry) = instructions.get(index) {
+            metadata_entries.push(*metadata_entry);
+        }
+
+        index += 1;
+    }
+
+    (
+        index,
+        Node {
+            id,
+            child_nodes,
+            metadata_entries,
+        },
+    )
+}
+
+fn build_node(instructions: &Vec<u8>) -> Node {
+    let (_, node) = build_node_recurse(instructions, 0, 0);
+
+    node
+}
+
+fn calc_node(node: &Node) -> u32 {
+    let name = (node.id as u8 + b'A') as char;
+
+    let node_value: u32 = match node.child_nodes.len() {
+        0 => node
+            .metadata_entries
+            .iter()
+            .map(|entry| *entry as u32)
+            .sum(),
+        _ => node
+            .metadata_entries
+            .iter()
+            .filter_map(|entry| node.child_nodes.get((entry - 1) as usize))
+            .map(|node| calc_node(node))
+            .sum(),
+    };
+
+    println!("{:2}--{:8}", name, node_value);
+
+    node_value
+}
+
+struct Node {
+    id: u8,
+    child_nodes: Vec<Node>,
+    metadata_entries: Vec<u8>,
+}
